ae4987bc2f7ee610fcc98d83029df9145f52d5b8
maichjospatoh-cmyk <maichjospatoh@gmail.com>
2026-04-02 15:59:19 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const upload = multer({ dest: "uploads/" });
 // Home page (UPLOAD UI)
 app.get("/", (req, res) => {
   res.send(`
+  <img src="https://raw.githubusercontent.com/maichjospatoh-cmyk/serial-insight-pro/main/WhatsApp%20Image%202026-01-14%20at%2012.38.50.jpeg" style="width:160px; display:block; margin:0 auto 15px;" />
     <h2>Serial Insight Pro</h2>
     <form action="/process" method="post" enctype="multipart/form-data">
       <input type="file" name="files" multiple required />
