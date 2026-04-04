874318733b724129f0ff1939f378cbb8491a0643
maichjospatoh-cmyk <maichjospatoh@gmail.com>
2026-04-04 13:58:43 +0300

    Update auth.js

--- backend/auth.js
+++ backend/auth.js
@@ -1,12 +1,9 @@
 const users = [
-  { username: "admin", password: "admin123", role: "admin" },
-  { username: "viewer", password: "viewer123", role: "viewer" }
+  { username: "admin", password: "admin123" }
 ];
 
 function authenticate(username, password) {
-  return users.find(
-    u => u.username === username && u.password === password
-  );
+  return users.find(u => u.username === username && u.password === password);
 }
 
 module.exports = { authenticate };
