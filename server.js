588187731e748e9a84a35e11e53333fafdfa1342
maichjospatoh-cmyk <maichjospatoh@gmail.com>
2026-04-04 16:04:54 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("SERVER IS WORKING ✅");
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log("Running"));
