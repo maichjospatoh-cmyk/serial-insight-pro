565caf75222d96e2b649b05d9ba231dbd5954b3c
maichjospatoh-cmyk <maichjospatoh@gmail.com>
2026-04-04 16:54:16 +0300

    Delete backend directory

--- backend/auth.js
+++
@@ -1,9 +0,0 @@
-const users = [
-  { username: "admin", password: "admin123" }
-];
-
-function authenticate(username, password) {
-  return users.find(u => u.username === username && u.password === password);
-}
-
-module.exports = { authenticate };
